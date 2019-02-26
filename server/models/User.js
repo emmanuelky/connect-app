b3504ac625f4c1b3cd6b580a3c829a19d3e8b181
Emmanuel Akhere Iyere <iyereemmanuelakhere@gmail.com>
2019-02-26 18:02:28 +0100

    created user models

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,65 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String,
-}, {
-    timestamps: {
-      createdAt: 'created_at',
-      updatedAt: 'updated_at'
-    }
-  });
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    university: {
+      type: String,
+      required: false
+    },
+    institute: {
+      type: String,
+      required: false
+    },
+    country: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    specialization: {
+      type: String,
+      required: true
+    },
+    status: {
+        type: String,
+        enum: ["alumni", "student"],
+      },
+    age: {
+      type: Number,
+      required: true
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female"]
+      },
+    social: [{
+        type: String,
+        url: String,
+      }],
+    },
 
-const User = mongoose.model('User', userSchema);
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
