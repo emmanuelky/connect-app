dac9d3db5797b54ec8cc6bf48a90894ec312939d
Emmanuel Akhere Iyere <iyereemmanuelakhere@gmail.com>
2019-03-03 16:01:35 +0100

    m

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import api from "../../api";
 import { Link } from "react-router-dom";
+import ControlledCarousel from './ControlledCarousel';
 
 class Home extends Component {
   constructor(props) {
@@ -27,65 +28,7 @@ class Home extends Component {
     return (
       <div className="Home p-3 mb-2 bg-dark text-white rounded-top ">
         
-        <div className="carousel">
-          <div
-            id="carouselExampleIndicators"
-            class="carousel slide"
-            data-ride="carousel"
-          >
-            <ol class="carousel-indicators">
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="0"
-                class="active"
-              />
-              <li data-target="#carouselExampleIndicators" data-slide-to="1" />
-              <li data-target="#carouselExampleIndicators" data-slide-to="2" />
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img
-                  class="d-block w-100"
-                  src="../../image2.jpeg"
-                  alt="First slide"
-                  width= "800px"
-                />
-              </div>
-              <div class="carousel-item">
-                <img
-                  class="d-block w-100"
-                  src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide"
-                  alt="Second slide"
-                />
-              </div>
-              <div class="carousel-item">
-                <img
-                  class="d-block w-100"
-                  src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide"
-                  alt="Third slide"
-                />
-              </div>
-            </div>
-            <a
-              class="carousel-control-prev"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="prev"
-            >
-              <span class="carousel-control-prev-icon" aria-hidden="true" />
-              <span class="sr-only">Previous</span>
-            </a>
-            <a
-              class="carousel-control-next"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="next"
-            >
-              <span class="carousel-control-next-icon" aria-hidden="true" />
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
+        <ControlledCarousel />
 
         <div>
           <h1 className="p-5">EXPLORE AND CONNECT</h1>
