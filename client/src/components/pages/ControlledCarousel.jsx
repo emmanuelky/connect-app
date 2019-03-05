bc778fcf99de94979aa03a72a88ffd102e5d2d5a
Emmanuel Akhere Iyere <iyereemmanuelakhere@gmail.com>
2019-03-05 19:21:14 +0100

    Fix for Heroku

--- client/src/components/pages/ControlledCarousel.jsx
+++ client/src/components/pages/ControlledCarousel.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Carousel from "react-bootstrap/Carousel";
+// import Carousel from "react-bootstrap/Carousel";
 import image1 from "../../image1.jpeg";
 import image2 from "../../image2.jpeg";
 import image3 from "../../image3.jpeg";
@@ -28,51 +28,51 @@ class ControlledCarousel extends Component {
     const { index, direction } = this.state;
 
     return (
-      <div />
-
-      <Carousel
-        activeIndex={index}
-        direction={direction}
-        onSelect={this.handleSelect}
-      >
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image1}
-            alt=""
-            style={{
-              width: "100vw",
-              height: "100vh"
-            }}
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image2}
-            alt=""
-            style={{
-              width: "100vw",
-              height: "100vh"
-            }}
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image3}
-            alt=""
-            style={{
-              width: "100vw",
-              height: "100vh"
-            }}
-          />
-        </Carousel.Item>
-      </Carousel>
+      <div>
+        {/* <Carousel
+          activeIndex={index}
+          direction={direction}
+          onSelect={this.handleSelect}
+        >
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image1}
+              alt=""
+              style={{
+                width: "100vw",
+                height: "100vh"
+              }}
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image2}
+              alt=""
+              style={{
+                width: "100vw",
+                height: "100vh"
+              }}
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={image3}
+              alt=""
+              style={{
+                width: "100vw",
+                height: "100vh"
+              }}
+            />
+          </Carousel.Item>
+        </Carousel> */}
+      </div>
     );
   }
 }
